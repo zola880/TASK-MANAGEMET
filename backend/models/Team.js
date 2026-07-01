43bc6c3ebd1026429a4193a0906164efaa4bc7e0
zola880 <zelalemybabe77@gmail.com>
2026-07-01 12:51:14 +0300

    removed the requred to be admin to create new team

--- backend/models/Team.js
+++ backend/models/Team.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },   
   secretKey: { type: String, required: true, unique: true },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 }, { timestamps: true });
